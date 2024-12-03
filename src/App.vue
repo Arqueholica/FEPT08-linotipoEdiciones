6f4f017031853a5129e43d79161f066ffda63de6
Arqueholica <ludmilaleboso@hotmail.com>
2024-12-03 11:18:37 +0100

    SecondCommitVueRouterOK

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
-  <div>
-    <h1 class="text-8xl text-red-600 font-bold underline">
-    Hello world! {{ title }} 🥸🍟🍔🛸
-  </h1>
-
+  <div class="grid grid-cols-3">
+    <div class=" col-start-1">
+      <h1 class="text-8xl text-red-600 font-bold underline">
+        Hello world! {{ title }} 🥸🍟🍔🛸
+      </h1>
+    </div>
+    <div class="col-start-2 p-10 font-bold text-serif"><router-view /></div>
   </div>
 </template>
 
-
-
-
-
 <script>
 export default {
-  name:"App",
-  data(){
-    return{
-      title: "LET'S EAT THE WORLD"
-    }
-  }
-  
-}
+  name: "App",
+  data() {
+    return {
+      title: "LET'S EAT THE WORLD",
+    };
+  },
+};
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Hello darkness my old friend 🛸
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+    name: "Home",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
