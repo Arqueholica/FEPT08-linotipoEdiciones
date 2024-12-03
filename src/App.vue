66c162fb1ca5f95e36483ea538299ff3aa937f4b
Arqueholica <ludmilaleboso@hotmail.com>
2024-12-03 10:33:42 +0100

    FirstCommit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="text-8xl text-red-600 font-bold underline">
+    Hello world! {{ title }} 🥸🍟🍔🛸
+  </h1>
+
+  </div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name:"App",
+  data(){
+    return{
+      title: "LET'S EAT THE WORLD"
+    }
+  }
+  
+}
+</script>
