c564d907f2d51d046cfdbdaf89c2b5ca04e1638a
Arqueholica <ludmilaleboso@hotmail.com>
2024-12-03 12:22:28 +0100

    FirstCommitViews

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid grid-cols-3">
-    <div class=" col-start-1">
+    <div class="col-start-1">
       <h1 class="text-8xl text-red-600 font-bold underline">
         Hello world! {{ title }} 🥸🍟🍔🛸
       </h1>
     </div>
-    <div class="col-start-2 p-10 font-bold text-serif"><router-view /></div>
+    <div class=" col-start-2 p-10 font-bold text-serif"><router-view /></div>
   </div>
 </template>
 
