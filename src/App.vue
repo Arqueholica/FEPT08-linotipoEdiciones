e1fee34c764fb6d9cb587f97a77af7a15f5aaae4
Arqueholica <ludmilaleboso@hotmail.com>
2024-12-05 13:24:06 +0100

    BaseEstaticaHomeViewAproxOk

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,71 @@
 <template>
-  <div class="grid grid-cols-3 grid-rows-3">
-    <div class="col-span-full row-start-1">
-      <div class="p-10 bg-blue-200">Yo soy tu home</div>
-     
+ 
+ <!-- TÍTULO CON LOGO + ENLACE EN LOGO (HOME) -->
+ 
+ <h1 class="px-20 py-2 flex text-8xl justify-center">
+    Linotipo
+    <router-link to="/"
+      ><img src="./assets/LogoLE.png" class="size-40"
+    /></router-link>
+    Ediciones
+  </h1>
+  
+  
+  <!-- BARRA DE NAVEGACIÓN -->
+  
+  <div class="p-8 bg-green-200 text-md font-bold font-serif">
+    <nav class="flex justify-start">
+      <router-link to="/colecciones"
+        ><button class="px-4">Colecciones</button></router-link
+      >
+      <router-link to="/fasciculos"
+        ><button class="px-4">Fascículos</button></router-link
+      >
+        <router-link to="/user"
+          ><button class="px-4"><img src="./assets/user.svg" /></button
+        ></router-link>
+    </nav>
+  </div>
+
+  <!-- ZONA DE EXPOSICIÓN DE VIEWS -->
+
+  <RouterView />
+
+
+  <!-- FOOTER CON INFO GENERAL -->
+
+  <footer class="grid grid-cols-3 gap-x-1">
+    <div class="col-span-1 bg-blue-200">
+      <h6 class="m-2 font-bold text-center"> CONTACTO</h6>
+      <input class="m-2" type="text" placeholder="María Josefa">
+      <input class= "w-[260px]" type="email" placeholder="mariajosefa@mail.com">
+      <textarea class="m-2 w-[450px] h-[100px]" placeholder="Escribe lo que necesites saber" />
     </div>
-    <div class="p-10 bg-green-200 col-span-full row-start-2">
-      <nav>
-        <router-link to="/colecciones"
-          ><button class="px-4">Colecciones</button></router-link
-        >
-        <router-link to="/fasciculos"
-          ><button class="px-4">Fascículos</button></router-link
-        >
-      </nav>
-    </div> 
-    <div class= "col-span-full row-start-3">
-    <RouterView />
-  </div>
-  </div>
+
+    <div class="col-span-1 bg-blue-200">
+      <h6 class="m-2 font-bold text-center"> MAPA DE LA WEB</h6>
+      <ul class="mx-2">
+        <li>Home</li>
+        <li>Colecciones</li>
+        <li>Fascículos</li>
+      </ul>
+    </div>
+
+    <div class="col-span-1 bg-blue-200">
+      <h6 class="m-2 font-bold text-center"> SÍGUENOS</h6>
+      <div class="flex justify-evenly"> 
+        <p> <a href=" https://www.instagram.com/" target="_blank">Instagram </a></p>
+        <p> <a href="https://www.facebook.com/" target="_blank">Facebook</a></p>
+        <p> <a href="https://www.tiktok.com/" target="_blank">TikTok</a></p>
+      </div>
+      <div class="flex flex-col justify-self-center my-4">
+      <p class="my-2"> ¿Quieres saber más de literatura? </p>
+      <button class="border-2 border-black rounded-full font-semibold text-lg">👉🏻 ¡Suscríbete! 👈🏻</button>
+    </div>
+    </div>
+
+
+  </footer>
 </template>
 
 <script>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,21 @@
 <template>
-    <div>
-        NOT FOUND
-    </div>
+<figure class="relative transition-all duration-300 hover:grayscale hover:scale-95">
+  <router-link to="/">
+    <img class="rounded-lg" src="../assets/CaravaggioNotFound.jpg">
+ </router-link>
+  <figcaption class="absolute px-4 text-xl font-serif text-white top-10 left-[500px]">
+      <p>Lo siento, no encuentro lo que buscas </p>
+  </figcaption>
+</figure>   
+
+
+
+
+
 </template>
+
+
+
 <script>
 export default {
     
